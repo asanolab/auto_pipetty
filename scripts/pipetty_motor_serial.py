3cdb58ff2c5995d213ba8fbdb660a993e52b9335
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2026-01-22 09:54:19 +0900

    add launch file

--- scripts/pipetty_motor_serial.py
+++ scripts/pipetty_motor_serial.py
@@ -3,20 +3,20 @@ import rospy
 import serial
 from std_msgs.msg import Bool
 
-class PipetteMotorNode:
+class PipettyMotorNode:
     def __init__(self):
         # 打开串口 ttyACM1，波特率 9600
-        port = rospy.get_param("~pipette_motor_port", "/dev/ttyACM1")
-        baud = int(rospy.get_param("~pipette_motor_baud", 9600))
+        port = rospy.get_param("~pipetty_motor_port", "/dev/ttyACM1")
+        baud = int(rospy.get_param("~pipetty_motor_baud", 9600))
         try:
             self.ser = serial.Serial(port, baud, timeout=1)
-            rospy.loginfo("Pipette motor connected on /dev/ttyACM1")
+            rospy.loginfo("Pipetty motor connected on /dev/ttyACM1")
         except Exception as e:
             rospy.logerr(f"Failed to open serial port: {e}")
             exit(1)
 
         # 创建订阅者
-        rospy.Subscriber('/pipette_motor/trigger', Bool, self.trigger_callback)
+        rospy.Subscriber('/pipetty_motor/trigger', Bool, self.trigger_callback)
 
     def trigger_callback(self, msg):
         if msg.data:  # 如果收到 True，就执行动作
@@ -24,7 +24,7 @@ class PipetteMotorNode:
                 command = '01'
                 command_byte = bytes.fromhex(command)
                 self.ser.write(command_byte)  # 发送字符 "1"
-                rospy.loginfo(f"Sent command to pipette motor: {command_byte} (eject tip)")
+                rospy.loginfo(f"Sent command to pipetty motor: {command_byte} (eject tip)")
             except Exception as e:
                 rospy.logwarn(f"Failed to send command: {e}")
 
@@ -33,6 +33,6 @@ class PipetteMotorNode:
 
 
 if __name__ == '__main__':
-    rospy.init_node('pipette_motor_node')
-    node = PipetteMotorNode()
+    rospy.init_node('pipetty_motor_node')
+    node = PipettyMotorNode()
     node.spin()
